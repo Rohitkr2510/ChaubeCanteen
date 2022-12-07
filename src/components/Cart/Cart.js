eff149904408065ecd53cb069000cae187275dca
Rohitkr2510 <rohit70_soe@jnu.ac.in>
2022-12-07 18:28:23 +0530

    first commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,21 +1,47 @@
-import { useContext } from "react";
-import classes from "./Cart.module.css";
+import React, { useContext, useState } from "react";
+
 import Modal from "../UI/Modal";
-import CartContext from "../../store/cart-context";
 import CartItem from "./CartItem";
+import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+import Checkout from "./Checkout";
 
 const Cart = (props) => {
+  const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSumbit, setDidSubmit] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `₹${cartCtx.totalAmount.toFixed(2)}`;
-  const hasItems = cartCtx.totalAmount > 0;
+  const hasItems = cartCtx.items.length > 0;
 
   const cartItemRemoveHandler = (id) => {
     cartCtx.removeItem(id);
   };
 
   const cartItemAddHandler = (item) => {
-    cartCtx.addItem({...item, amount:1})
+    cartCtx.addItem(item);
+  };
+
+  const orderHandler = () => {
+    setIsCheckout(true);
+  };
+
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    await fetch(
+      "https://chaubecanteen-default-rtdb.firebaseio.com/orders.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          user: userData,
+          orderedItems: cartCtx.items,
+        }),
+      }
+    );
+    setIsSubmitting(false);
+    setDidSubmit(true);
+    cartCtx.clearCart();
   };
 
   const cartItems = (
@@ -26,28 +52,58 @@ const Cart = (props) => {
           name={item.name}
           amount={item.amount}
           price={item.price}
-          onRemove = {cartItemRemoveHandler.bind(null, item.id)}
-          onAdd = {cartItemAddHandler.bind(null,item)}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
         />
       ))}
     </ul>
   );
 
-  return (
-    <Modal onClose={props.onClose}>
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes["button--alt"]} onClick={props.onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
+  const cartModelContent = (
+    <React.Fragment>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onClose}>
-          Close
-        </button>
-        {hasItems && <button className={classes.button}>Order</button>}
-      </div>
-    </Modal>
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
+      {!isCheckout && modalActions}
+    </React.Fragment>
   );
+
+  const isSubmittingModalContent = <p>Sending order data...</p>
+
+  const didSubmitModalContent =<React.Fragment>
+    <p>Successfully sent the order</p>
+    <div className={classes.actions}>
+      <button className={classes.button} onClick={props.onClose}>
+        Close
+      </button>
+      </div>
+
+
+  </React.Fragment>  
+
+  return <Modal onClose={props.onClose}>
+    {!isSubmitting && !didSumbit &&  cartModelContent}
+    {isSubmitting && isSubmittingModalContent}
+    {!isSubmitting && didSumbit && didSubmitModalContent}
+  </Modal>;
 };
 
 export default Cart;
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,210 +1,65 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Tea",
-    description: "Smell grassy, light and fresh ",
-    price: 10,
-  },
-  {
-    id: "m2",
-    name: "Black Tea",
-    description: "true tea that comes from the Camellia sinensis plant!",
-    price: 8,
-  },
-  {
-    id: "m3",
-    name: "Lemon Tea",
-    description: "a refreshing tea",
-    price: 10,
-  },
-  {
-    id: "m4",
-    name: "coffee",
-   // description: "Healthy...and green...",
-    price: 12,
-  },
-  {
-    id: "m9",
-    name: "Samosa",
-    description: "a fried or baked pastry with a savory filling",
-    price: 8,
-  },
-  {
-    id: "m14",
-    name: "Bread Pakoda",
-   // description: "Healthy...and green...",
-    price: 10,
-  },
-  {
-    id: "m5",
-    name: "Sandwitch",
-   //description: "Healthy...and green...",
-    price: 15,
-  },
-  {
-    id: "m6",
-    name: "Single Egg Omlet",
-   // description: "Healthy...and green...",
-    price: 15,
-  },
-  {
-    id: "m7",
-    name: "Double Egg Omlet",
-   // description: "Healthy...and green...",
-    price: 25,
-  },
-  {
-    id: "m8",
-    name: "Bread Omlet",
-   // description: "Healthy...and green...",
-    price: 20,
-  },
-  {
-    id: "m10",
-    name: "Double Omlet",
-   // description: "Healthy...and green...",
-    price: 30,
-  },
-  {
-    id: "m11",
-    name: "Veg Maggi",
-   // description: "Healthy...and green...",
-    price: 30,
-  },
-  {
-    id: "m12",
-    name: "Egg Maggi",
-   // description: "Healthy...and green...",
-    price: 40,
-  },
-  {
-    id: "m13",
-    name: "Fry Maggi",
-   // description: "Healthy...and green...",
-    price: 35,
-  },
-  {
-    id: "m15",
-    name: "Aloo Paratha",
-   // description: "Healthy...and green...",
-    price: 15,
-  },
-  {
-    id: "m16",
-    name: "Pyaz Paratha",
-   // description: "Healthy...and green...",
-    price: 15,
-  },
-  {
-    id: "m17",
-    name: "Paneer Paratha",
-   // description: "Healthy...and green...",
-    price: 25,
-  },
-  {
-    id: "m18",
-    name: "Egg Paratha",
-   // description: "Healthy...and green...",
-    price: 20,
-  },
-  {
-    id: "m19",
-    name: "Double Egg Paratha",
-   // description: "Healthy...and green...",
-    price: 30,
-  },
-  {
-    id: "m20",
-    name: "Veg Momos (Full Plate)",
-   // description: "Healthy...and green...",
-    price: 50,
-  },
-  {
-    id: "m21",
-    name: "Veg Momos (Half Plate)",
-   // description: "Healthy...and green...",
-    price: 25,
-  },
-  {
-    id: "m22",
-    name: "Non Veg Momos (Full Plate)",
-  //  description: "Healthy...and green...",
-    price: 60,
-  },
-  {
-    id: "m23",
-    name: "Non Veg Momos (Half Plate)",
-   // description: "Healthy...and green...",
-    price: 30,
-  },
-  {
-    id: "m24",
-    name: "Panner Momos (Full Plate)",
-  //  description: "Healthy...and green...",
-    price: 60,
-  },
-  {
-    id: "m25",
-    name: "Paneer Momos (Half Plate)",
-  //  description: "Healthy...and green...",
-    price: 30,
-  },
-  {
-    id: "m26",
-    name: "Chowmin (Full Plate)",
-  //  description: "Healthy...and green...",
-    price: 50,
-  },
-  {
-    id: "m27",
-    name: "Chowmin (Half Plate)",
-  //  description: "Healthy...and green...",
-    price: 30,
-  },
-  {
-    id: "m28",
-    name: "Single Egg Chowmin (Full Plate)",
-  //  description: "Healthy...and green...",
-    price: 60,
-  },
-  {
-    id: "m29",
-    name: "Single Egg Chowmin (Half Plate)",
-  //  description: "Healthy...and green...",
-    price: 40,
-  },
-  {
-    id: "m30",
-    name: "Fried Rice (Full Plate)",
-  //  description: "Healthy...and green...",
-    price: 50,
-  },
-  {
-    id: "m31",
-    name: "Fried Rice (Half Plate)",
-   // description: "Healthy...and green...",
-    price: 30,
-  },
-  {
-    id: "m32",
-    name: "Egg Fried Rice (Full Plate)",
-  //  description: "Healthy...and green...",
-    price: 60,
-  },
-  {
-    id: "m33",
-    name: "Egg Fried Rice (Half Plate)",
-  //  description: "Healthy...and green...",
-    price: 40,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsloading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://chaubecanteen-default-rtdb.firebaseio.com/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      console.log(loadedMeals);
+
+      setMeals(loadedMeals);
+      setIsloading(false);
+    };
+
+    fetchMeals().catch((error) => {
+      setIsloading(false);
+      setHttpError(error.message);
+    });
+
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
